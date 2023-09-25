6eff7515c705db6cc220a39e8c89509bdc6f4cb0
MeriliisT11 <m3riliis.toom3t@gmail.com>
2023-09-25 10:29:23 +0300

    Add flag work

--- script.js
+++ script.js
@@ -2,18 +2,39 @@
 // setTimeout(() => {
 //    document.location = 'https://google.com';
 // }, 5000);
-let canvas = document.querySelector( 'canvas');
-let ctx = canvas.getContext('2d');
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 250, 50);
+ctx.fillStyle = "black";
+ctx.fillRect(0, 50, 250, 50);
+
+ctx.strokeStyle = "black";
+ctx.strokeRect(0, 0, 250, 300);
+
 ctx.beginPath();
-ctx.arc(100,100, 50, 0, 2 * Math.PI);
-ctx.strokeStyle = 'green'
-ctx.lineWidth = 1;
+ctx.moveTo(370, 50); // Move pen to bottom-left corner
+ctx.lineTo(400,10); // Line to top corner
+ctx.lineTo(430, 50); // Line to bottom-right corner
+ctx.closePath(); // Line to bottom-left corner
 ctx.stroke();
-ctx.font = "30px Arial";
-ctx.fillText('Hello Canvas', 30, 50);
-ctx.strokeText('Hello Canvas', 30, 100);
-ctx.fillStyle = '#ff0';
-ctx.fillRect(10, 10, 10, 10);
+
+ctx.beginPath();
+ctx.moveTo(370, 25); // Move pen to bottom-left corner
+ctx.lineTo(400, 64); // Line to top corner
+ctx.lineTo(430, 25); // Line to bottom-right corner
+ctx.closePath(); // Line to bottom-left corner
+ctx.stroke();
+
+// Create circular clipping region
+ctx.beginPath();
+ctx.arc(400, 110, 30, 0, Math.PI * 2);
+ctx.clip();
+
+// Draw stuff that gets clipped
+ctx.fillStyle = "green";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "pink";
+ctx.fillRect(401, 15, 30, 95);
+
